0878fb5a5525d9137c87a7b69afbb478381eec78
nickern <nickern@gmail.com>
2017-02-12 01:19:21 +0100

    Moved routes to seperate file

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var config = require('./config');
-
-// Load controllers
-var itemController = require('./controllers/items')
+var routes = require('./routes');
 
 // Connect to the database
 mongoose.Promise = require('bluebird');
@@ -15,31 +13,12 @@ mongoose.connect('mongodb://' + config.database.host + ':' + config.database.por
 var app = express();
 
 // Use the body-parser package in our application
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.json({
   extended: true
 }));
 
-var port = config.web.port;
-var router = express.Router();
-
-// Initial dummy route for testing
-router.get('/', function(req, res) {
-  res.json({ message: 'Hello World!!' });
-});
-
-// Create endpoint prefix /items
-router.route('/items')
-  .post(itemController.postItems)
-  .get(itemController.getItems);
-
-// Create endpoint prefix /items/:item_id
-router.route('/items/:item_id')
-  .get(itemController.getItem)
-  .put(itemController.putItem)
-  .delete(itemController.deleteItem);
-
-app.use(config.web.url, router);
+app.use(config.web.url, routes);
 
 // Start the server
-app.listen(port);
-console.log('API ready! Listening on port: ' + port);
+app.listen(config.web.port);
+console.log('API ready! Listening on port: ' + config.web.port);
