f1f0e862cb1006ae46dd3995b77861e51cf17567
nickern <nickern@gmail.com>
2017-02-16 23:07:57 +0100

    Added basic file structure

--- config/auth.js
+++ config/auth.js
@@ -13,8 +13,14 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
+        'clientID'      : '660119170909-shhf4ropv2qf0f1fc3v1s1aubo4cqpgj.apps.googleusercontent.com',
+        'clientSecret'  : 'Oq6T33F77QBdvgmwh0PWETrX',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    },
+
+    'dataporten' : {
+        'clientID'      : '660119170909-shhf4ropv2qf0f1fc3v1s1aubo4cqpgj.apps.googleusercontent.com',
+        'clientSecret'  : 'Oq6T33F77QBdvgmwh0PWETrX',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
